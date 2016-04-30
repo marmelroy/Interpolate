d4212af9818c09f58d5ccd2902e16988784cba76
Roy Marmelstein <marmelroy@gmail.com>
2016-04-30 14:16:45 +0200

    Better example

--- examples/Animations/Sample/Sample/ViewController.swift
+++ examples/Animations/Sample/Sample/ViewController.swift
@@ -11,20 +11,57 @@ import Interpolate
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var animationView: UIView!
+    
+    var colorChange: Interpolate?
+    var radiusChange: Interpolate?
+    var sizeChange: Interpolate?
+
+    var animationViewCenter = CGPoint.zero
+    
+    @IBAction func didPan(sender: AnyObject) {
+        let gestureRecognizer = sender as? UIPanGestureRecognizer
+        if let translatedPoint = gestureRecognizer?.translationInView(self.view) {
+            if gestureRecognizer?.state == .Began {
+                animationViewCenter = animationView.center
+            }
+            animationView.center = CGPointMake(animationViewCenter.x, animationViewCenter.y + translatedPoint.y)
+            let progress = animationView.center.y / self.view.frame.size.height
+            colorChange?.progress = progress
+            radiusChange?.progress = progress
+            var sizeProgress = animationView.center.y / self.view.center.y
+            if sizeProgress > 1 {
+                sizeProgress = 2 - sizeProgress
+            }
+            sizeChange?.progress = sizeProgress
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let squareView = UIView()
-        squareView.backgroundColor = UIColor.greenColor()
-        self.view.addSubview(squareView)
-        squareView.frame.size = CGSizeMake(100, 100)
-        squareView.center = self.view.center
-        let positionChange = Interpolate(from: self.view.center, to: CGPointMake(50, 50), apply: { (result) in
-            if let center = result as? CGPoint {
-                squareView.center = center
+        colorChange = Interpolate(from: UIColor.redColor(), to: UIColor.blueColor(), apply: { [weak self] (result) in
+            if let color = result as? UIColor {
+                self?.animationView.backgroundColor = color
             }
-        }, function: SpringInterpolation(damping: 10.0, velocity: 0.0, mass: 1.0, stiffness: 100.0))
-        positionChange.animate(1.0)
-
+        })
+        radiusChange = Interpolate(from: 0.0, to: 20, apply: { [weak self] (result) in
+            if let radius = result as? CGFloat {
+                self?.animationView.layer.cornerRadius = radius
+            }
+        })
+        sizeChange = Interpolate(from: CGSizeMake(40, 40), to: CGSizeMake(300, 300), apply: { [weak self] (result) in
+            if let size = result as? CGSize {
+                self?.animationView.frame.size = size
+            }
+        }, function: BasicInterpolation.EaseIn)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let progress = animationView.center.y / self.view.frame.size.height
+        colorChange?.progress = progress
+        radiusChange?.progress = progress
+        sizeChange?.progress = 1.0
     }
 
     override func didReceiveMemoryWarning() {
