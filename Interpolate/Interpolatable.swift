c1b17901e234db1c23c22a309bb31847a72099c1
Roy Marmelstein <marmelroy@gmail.com>
2016-05-08 14:06:53 +0200

    Adding support for UIEdgeInsets

--- Interpolate/Interpolatable.swift
+++ Interpolate/Interpolatable.swift
@@ -19,36 +19,71 @@ public protocol Interpolatable {
 /**
  Supported interpolatable types.
 
+ - CATransform3D:       CATransform3D type
+ - CGAffineTransform:   CGAffineTransform type.
+ - CGFloat:             CGFloat type.
  - CGPoint:             CGPoint type.
  - CGRect:              CGRect type.
  - CGSize:              CGSize type.
+ - ColorHSB:            ColorHSB type.
+ - ColorMonochrome:     ColorMonochrome type.
+ - ColorRGB:            ColorRGB type.
  - Double:              Double type.
- - CGFloat:             CGFloat type.
  - Int:                 Int type.
  - NSNumber:            NSNumber type.
- - ColorRGB:            ColorRGB type.
- - ColorMonochrome:     ColorMonochrome type.
- - ColorHSB:            ColorHSB type.
- - CGAffineTransform:   CGAffineTransform type.
- - CATransform3D:       CATransform3D type
+ - UIEdgeInsets:        UIEdgeInsets type.
  */
 
 public enum InterpolatableType {
+    case CATransform3D
+    case CGAffineTransform
+    case CGFloat
     case CGPoint
     case CGRect
     case CGSize
+    case ColorHSB
+    case ColorMonochrome
+    case ColorRGB
     case Double
-    case CGFloat
     case Int
     case NSNumber
-    case ColorRGB
-    case ColorMonochrome
-    case ColorHSB
-    case CGAffineTransform
-    case CATransform3D
+    case UIEdgeInsets
 }
 
 // MARK: Extensions
+
+extension CATransform3D: Interpolatable {
+    /**
+     Vectorize CATransform3D.
+     
+     - returns: IPValue
+     */
+    public func vectorize() -> IPValue {
+        return IPValue(type: .CATransform3D, vectors: [m11, m12, m13, m14, m21, m22, m23, m24, m31, m32, m33, m34, m41, m42, m43, m44])
+    }
+}
+
+extension CGAffineTransform: Interpolatable {
+    /**
+     Vectorize CGAffineTransform.
+     
+     - returns: IPValue
+     */
+    public func vectorize() -> IPValue {
+        return IPValue(type: .CGAffineTransform, vectors: [a, b, c, d, tx, ty])
+    }
+}
+
+extension CGFloat: Interpolatable {
+    /**
+     Vectorize CGFloat.
+     
+     - returns: IPValue
+     */
+    public func vectorize() -> IPValue {
+        return IPValue(type: .CGFloat, vectors: [self])
+    }
+}
 
 extension CGPoint: Interpolatable {
     /**
@@ -83,27 +118,6 @@ extension CGSize: Interpolatable {
     }
 }
 
-extension NSNumber: Interpolatable {
-    /**
-     Vectorize NSNumber.
-     
-     - returns: IPValue
-     */
-    public func vectorize() -> IPValue {
-        return IPValue(type: .NSNumber, vectors: [CGFloat(self)])
-    }
-}
-
-extension Int: Interpolatable {
-    /**
-     Vectorize Int.
-     
-     - returns: IPValue
-     */
-    public func vectorize() -> IPValue {
-        return IPValue(type: .Int, vectors: [CGFloat(self)])
-    }
-}
 
 extension Double: Interpolatable {
     /**
@@ -116,36 +130,26 @@ extension Double: Interpolatable {
     }
 }
 
-extension CGFloat: Interpolatable {
+
+extension Int: Interpolatable {
     /**
-     Vectorize CGFloat.
+     Vectorize Int.
      
      - returns: IPValue
      */
     public func vectorize() -> IPValue {
-        return IPValue(type: .CGFloat, vectors: [self])
+        return IPValue(type: .Int, vectors: [CGFloat(self)])
     }
 }
 
-extension CGAffineTransform: Interpolatable {
+extension NSNumber: Interpolatable {
     /**
-     Vectorize CGAffineTransform.
+     Vectorize NSNumber.
      
      - returns: IPValue
      */
     public func vectorize() -> IPValue {
-        return IPValue(type: .CGAffineTransform, vectors: [a, b, c, d, tx, ty])
-    }
-}
-
-extension CATransform3D: Interpolatable {
-    /**
-     Vectorize CATransform3D.
-     
-     - returns: IPValue
-     */
-    public func vectorize() -> IPValue {
-        return IPValue(type: .CATransform3D, vectors: [m11, m12, m13, m14, m21, m22, m23, m24, m31, m32, m33, m34, m41, m42, m43, m44])
+        return IPValue(type: .NSNumber, vectors: [CGFloat(self)])
     }
 }
 
@@ -176,6 +180,18 @@ extension UIColor: Interpolatable {
     }
 }
 
+extension UIEdgeInsets: Interpolatable {
+    /**
+     Vectorize UIEdgeInsets.
+     
+     - returns: IPValue
+     */
+    public func vectorize() -> IPValue {
+        return IPValue(type: .UIEdgeInsets, vectors: [top, left, bottom, right])
+    }
+}
+
+
 public class IPValue {
     
     let type: InterpolatableType
@@ -188,30 +204,32 @@ public class IPValue {
     
     func toInterpolatable() -> Interpolatable {
         switch type {
+            case .CATransform3D:
+                return CATransform3D(m11: vectors[0], m12: vectors[1], m13: vectors[2], m14: vectors[3], m21: vectors[4], m22: vectors[5], m23: vectors[6], m24: vectors[7], m31: vectors[8], m32: vectors[9], m33: vectors[10], m34: vectors[11], m41: vectors[12], m42: vectors[13], m43: vectors[14], m44: vectors[15])
+            case .CGAffineTransform:
+                return CGAffineTransform(a: vectors[0], b: vectors[1], c: vectors[2], d: vectors[3], tx: vectors[4], ty: vectors[5])
+            case .CGFloat:
+                return vectors[0]
             case .CGPoint:
                 return CGPoint(x: vectors[0], y: vectors[1])
             case .CGRect:
                 return CGRect(x: vectors[0], y: vectors[1], width: vectors[2], height: vectors[3])
             case .CGSize:
                 return CGSize(width: vectors[0], height: vectors[1])
-            case .Double:
-                return vectors[0]
-            case .CGFloat:
-                return vectors[0]
-            case .Int:
-                return vectors[0]
-            case .NSNumber:
-                return vectors[0]
             case .ColorRGB:
                 return UIColor(red: vectors[0], green: vectors[1], blue: vectors[2], alpha: vectors[3])
             case .ColorMonochrome:
                 return UIColor(white: vectors[0], alpha: vectors[1])
             case .ColorHSB:
                 return UIColor(hue: vectors[0], saturation: vectors[1], brightness: vectors[2], alpha: vectors[3])
-            case .CGAffineTransform:
-                return CGAffineTransform(a: vectors[0], b: vectors[1], c: vectors[2], d: vectors[3], tx: vectors[4], ty: vectors[5])
-            case .CATransform3D:
-                return CATransform3D(m11: vectors[0], m12: vectors[1], m13: vectors[2], m14: vectors[3], m21: vectors[4], m22: vectors[5], m23: vectors[6], m24: vectors[7], m31: vectors[8], m32: vectors[9], m33: vectors[10], m34: vectors[11], m41: vectors[12], m42: vectors[13], m43: vectors[14], m44: vectors[15])
+            case .Double:
+                return vectors[0]
+            case .Int:
+                return vectors[0]
+            case .NSNumber:
+                return vectors[0]
+            case .UIEdgeInsets:
+                return UIEdgeInsetsMake(vectors[0], vectors[1], vectors[2], vectors[3])
         }
     }
     
