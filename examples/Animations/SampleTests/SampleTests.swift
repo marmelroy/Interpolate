1f286b002ba0ad45b48987372e997308b557b151
Roy Marmelstein <marmelroy@gmail.com>
2016-08-27 06:28:13 +0300

    Fix example so that it works with Swif 3.0

--- examples/Animations/SampleTests/SampleTests.swift
+++ examples/Animations/SampleTests/SampleTests.swift
@@ -7,7 +7,6 @@
 //
 
 import XCTest
-@testable import Sample
 
 class SampleTests: XCTestCase {
     
@@ -28,7 +27,7 @@ class SampleTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
